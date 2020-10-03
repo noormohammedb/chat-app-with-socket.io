b8cce7363cb7ac362b8dc052d9b928c8336acd3b
noormohammedb <nooru.mohad@gmail.com>
2020-10-03 18:55:14 +0530

    MVP dom manipulation restricted multiple send all messages containerized

--- public/javascripts/client-script.js
+++ public/javascripts/client-script.js
@@ -1,17 +1,43 @@
-var socket = io();
-
 console.log('javascript runnig');
 
-document.getElementById('button-addon2').addEventListener('click',(e)=>{
-   e.preventDefault();
-   message = document.getElementById('message-box').value
-   console.log('clicked  ', message);
+var socket = io();
 
-   socket.emit('chat_send', message)
+
+socket.on('connect', () => {
+   console.log('socket connection');
+});
+
+
+document.getElementById('button-addon2').addEventListener('click', (e) => {
+   e.preventDefault();
+   userMessage = document.getElementById('message-box')
+   console.log('clicked  ', userMessage.value);
+   if(userMessage.value)
+   socket.emit('chat_send', userMessage.value)
+   userMessage.value = '';
+
 })
 
+socket.on('chat_receive',(socket_message)=>{
+   push({message:socket_message})
+})
 
-socket.on('connect',()=>{
-   console.log('socket connection');
+let wrapClone
+let wrapDiv = document.getElementsByClassName('wrap')[0];
+let con = document.getElementById('message-container');
 
-});
+let push = (data) => {
+   // wrapClone = wrapDiv.cloneNode(true)
+   var ele = document.createElement('div');
+   ele.classList = 'wrap';
+   ele.innerHTML = `
+                  <h5 class="display-6" id="chat-username">username {{username}} </h5>
+                  <p class="lead" id="chat-message">${data.message}</p>
+                  <p> time {{time}} </p>`
+   con.prepend(ele)
+}
+
+// push({message:'heoo'});
+// push({message:'loo'});
+
+
--- socketApi.js
+++ socketApi.js
@@ -7,7 +7,10 @@ socketApi.io = io;
 io.on('connection',socket=>{
    console.log('socket connection');
 
-   socket.on('chat_send',console.log)
+   socket.on('chat_send',(data)=>{
+      console.log(data);
+      io.sockets.emit('chat_receive',data)
+   })
 
 });
 
